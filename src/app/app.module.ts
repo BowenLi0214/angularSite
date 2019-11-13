f011df2763a33dd29e24522c7dbe0195d24e3535
Bowen Li <bowen.li@mindsea.com>
2019-11-13 12:27:24 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SubPageComponent } from "./sub-page/sub-page.component";
+import { RouterModule } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { MatButtonModule } from "@angular/material/button";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SubPageComponent, HomePageComponent],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot([
+      { path: "", component: HomePageComponent },
+      { path: "subpage", component: SubPageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
